c77b2942c6b50f0a6bca0a4cf63eb76f68eb6158
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-11-27 20:33:51 +0600

    Request data read

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const teacherCollection = client.db("schoolDB").collection("request");
 
@@ -36,8 +36,15 @@ async function run() {
             res.send(result);
         })
 
+        // Read Request
+        app.get('/request', async (req, res) => {
+            const cursor = teacherCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
