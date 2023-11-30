ff07170ac7d5c6b5e3e4631eb13dfee232887436
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-11-30 23:16:59 +0600

    Final commit

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ async function run() {
             res.send(result);
         })
 
-        // Get Update
+        // Get 
         app.get('/request/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
